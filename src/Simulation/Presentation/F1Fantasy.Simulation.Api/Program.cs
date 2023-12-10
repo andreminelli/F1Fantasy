b7dc156f117d23d806107ba797dc36e15bf1d553
André Minelli <andreminelli@gmail.com>
2023-12-09 21:13:13 -0300

    Use native JWT Bearer authentication

--- src/Simulation/Presentation/F1Fantasy.Simulation.Api/Program.cs
+++ src/Simulation/Presentation/F1Fantasy.Simulation.Api/Program.cs
@@ -3,9 +3,7 @@ using F1Fantasy.Simulation.Application;
 using F1Fantasy.Simulation.Infrastructure;
 using FastEndpoints;
 using FastEndpoints.Swagger;
-using Keycloak.AuthServices.Authentication;
-using Keycloak.AuthServices.Authorization;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,23 +14,12 @@ builder.Services
 
 builder.Services
     .AddFastEndpoints()
-    .AddAuthorization()
     .SwaggerDocument();
 
 builder.Services
-    .AddKeycloakAuthentication(builder.Configuration, options =>
-    {
-        options.Authority = "http://keycloak:8080/realms/F1Fantasy";
-        options.Audience = "api-simulation";
-        options.RequireHttpsMetadata = false; // Development only!!
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidIssuer = "http://localhost:8888/realms/F1Fantasy",
-            ValidAudience = "api-simulation",
-            
-        };
-    });
-builder.Services.AddKeycloakAuthorization(builder.Configuration);
+    .AddAuthentication()
+    .AddJwtBearer();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
